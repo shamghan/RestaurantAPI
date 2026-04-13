117e679a3dd217b95c5fd734b5890b630732b7a2
gkashyup <162294431+gkashyup@users.noreply.github.com>
2026-04-13 23:51:41 +0530

    add value in claims

--- Restaurants.Infrastructure/Extension/ServiceCollectionExtensions.cs
+++ Restaurants.Infrastructure/Extension/ServiceCollectionExtensions.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Restaurants.Domain.Entities;
 using Restaurants.Domain.Repositories;
+using Restaurants.Infrastructure.Authorization;
 using Restaurants.Infrastructure.Persistence;
 using Restaurants.Infrastructure.Repositories;
 using Restaurants.Infrastructure.Seeders;
@@ -17,8 +18,9 @@ public static class ServiceCollectionExtensions
         services.AddDbContext<RestaurantsDbContext>(options => options.UseSqlServer(connectionString)
         .EnableSensitiveDataLogging()
         );
-        services.AddIdentityApiEndpoints<User>().AddRoles<IdentityRole>().
-            AddEntityFrameworkStores<RestaurantsDbContext>();//.AddRoles<IdentityRole>(); allows user role management for authorization. Without it, accessing role-protected resources can trigger a 403 Forbidden error.
+        services.AddIdentityApiEndpoints<User>().AddRoles<IdentityRole>()
+            .AddClaimsPrincipalFactory< RestaurantsUserClaimsPrincipleFactory>()
+            .AddEntityFrameworkStores<RestaurantsDbContext>();//.AddRoles<IdentityRole>(); allows user role management for authorization. Without it, accessing role-protected resources can trigger a 403 Forbidden error.
         services.AddScoped<IRestaurantsSeeder, RestaurantsSeeder>();
         services.AddScoped<IRestaurantsRepository, RestaurantsRepository>();
         services.AddScoped<IDishesRepository, DishesRepository>();
