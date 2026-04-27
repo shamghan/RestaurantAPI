d6f94cea4b36898038ebf8580c969cee886f2c7a
gkashyup <162294431+gkashyup@users.noreply.github.com>
2026-04-28 00:01:46 +0530

    Pagination request validation

---
+++ Restaurants.Application/Restaurants/Queries/GetAllRestaurants/GetAllRestaurantQueryValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurants.Application.Restaurants.Queries.GetAllRestaurants
+{
+    public class GetAllRestaurantQueryValidator : AbstractValidator<GetAllRestaurantQuery>
+    {
+        private  int[] allowedPageSize = [5,10,15,30];
+        public GetAllRestaurantQueryValidator()
+        {
+            RuleFor(x => x.PageNumber)
+                .GreaterThanOrEqualTo(1);
+            RuleFor(x => x.PageSize)
+                .Must(x => allowedPageSize.Contains(x))
+                .WithMessage($"Page size must be one of the following: {string.Join(", ", allowedPageSize)}");
+        }
+    }
+}
