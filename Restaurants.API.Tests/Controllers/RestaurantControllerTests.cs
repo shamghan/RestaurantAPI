82eb25528267afdfec15201fd4b9f37229265864
gkashyup <162294431+gkashyup@users.noreply.github.com>
2026-05-10 00:17:43 +0530

    Controller tests

---
+++ Restaurants.API.Tests/Controllers/RestaurantControllerTests.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurants.API.Tests.Controllers
+{
+    public class RestaurantControllerTests(WebApplicationFactory<Program> factory) : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _factory = factory;
+
+        [Fact()]
+        public async Task GetAll_ForValidRequest_Returns200Ok()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+            // Act
+            var result = await client.GetAsync("/api/restaurants?pageNumber=1&pageSize=10");
+            // Assert
+            result.StatusCode.Should().Be(System.Net.HttpStatusCode.OK); // Status Code 200-299
+        }
+        [Fact()]
+        public async Task GetAll_ForInValidRequest_Returns400BadRequest()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+            // Act
+            var result = await client.GetAsync("/api/restaurants");
+            // Assert
+            result.StatusCode.Should().Be(System.Net.HttpStatusCode.BadRequest); // Status Code 200-299
+        }
+    }
+}
